a911373de5537f34b8010680deea2ba631253a90
alekospagon <56389540+alekospagon@users.noreply.github.com>
2020-02-12 02:06:48 -0500

    Update compare.sh

--- ASKHSH 6/compare.sh
+++ ASKHSH 6/compare.sh
@@ -1,8 +1,8 @@
 echo "Time of single core:"
-time (cat samples.txt) | ./a
+time (cat samples.txt) | ./naive_not_parallel
 
 echo "Time of parallel:"
-time (cat samples.txt) | ./parallel +RTS -N4
+time (cat samples.txt) | ./naive_parallel_rpar +RTS -N4
 
 echo "Time of ultra:"
-time (cat samples.txt) | ./ultra +RTS -N8
+time (cat samples.txt) | ./four_parallel_par_monad +RTS -N8
